62917e47b3d87b11390951eee803c94cd9ba0fdd
Florian Meier <Florian.Meier@hawo.net>
2020-06-11 16:29:08 +0200

    Release 3.42

--- ItemRack/ItemRack.lua
+++ ItemRack/ItemRack.lua
@@ -3,7 +3,7 @@ ItemRack = {}
 local disable_delayed_swaps = nil -- temporary. change nil to 1 to stop attempting to delay set swaps while casting
 local _
 
-ItemRack.Version = "3.41"
+ItemRack.Version = "3.42"
 
 ItemRackUser = {
 	Sets = {}, -- user's sets
